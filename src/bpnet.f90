7484475a5c317cb83ea16897c6b65264e71795d7
cometscome <cometscome@gmail.com>
2025-02-07 12:34:39 +0900

    first commit

--- src/bpnet.f90
+++ src/bpnet.f90
@@ -1,6 +1,8 @@
 module bpnet
     use shared_potentials,only:initialize_potentials,&
-            bpnet_atomic_energy_and_forces_novirial,set_Rcmax_and_Rcmin,&
+            bpnet_atomic_energy_and_forces_novirial,&
+            set_Rcmax_and_Rcmin,&
             deallocate_potentials,reload_potentials
+    use sub_generate,only:generate_subroutine
     implicit none
 end module
